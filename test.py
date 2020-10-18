7fed22ff11e5c3a7c04a710ed7bdc69ed976e44a
Michaela Matocha <mich-matocha@users.noreply.github.com>
2020-10-18 00:54:54 -0500

    Adding titles and radio button

--- test.py
+++ test.py
@@ -6,24 +6,41 @@ import pandas as pd
 import time
 
 import plotly.figure_factory as ff
+import plotly.express as px
+
+st.title('Texas Hidden Talent')
+graph_type = st.radio('Metric', [1,2])
 
 df_sample = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/minoritymajority.csv')
 df_sample_r = df_sample[df_sample['STNAME'] == 'Texas']
 fips = df_sample_r['FIPS'].tolist()
 
 values = df_sample_r['TOT_POP'].tolist()
-
-
 endpts = list(np.mgrid[min(values):max(values):4j])
+
 colorscale = ["#030512","#1d1d3b","#323268","#3d4b94","#3e6ab0",
               "#4989bc","#60a7c7","#85c5d3","#b7e0e4","#eafcfd"]
+
+if graph_type == 1:
+    colorscale = px.colors.sequential.Aggrnyl
+    # values = df_sample_r['metric1']
+    # endpts = list(np.mgrid[min(values):max(values):4j])
+else:
+    colorscale = px.colors.sequential.Agsunset
+    # values = df_sample_r['metric2']
+    # endpts = list(np.mgrid[min(values):max(values):4j])
+
+
+
+
 fig = ff.create_choropleth(
     fips=fips, values=values, scope=['Texas'], show_state_data=True,
     colorscale=colorscale, binning_endpoints=endpts, round_legend_values=True,
     legend_title='Population by County',
     county_outline={'color': 'rgb(255,255,255)', 'width': 0.5},
-    exponent_format=True,
-)
+    exponent_format=True
+    )
+
 fig.layout.template = None
 
 st.write(fig)
